f56158ed6445ba167de27ab12e585641b743aa60
Sam Groves <samuelhgroves@gmail.com>
2023-03-28 21:17:28 -0400

    Added ability to add tasks by project

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
+import "./style.css";
+
 const projectsArr = [];
+let activeProject = null;
+function findActiveProject() {
+  for (let i = 0; i < projectsArr.length; i++) {
+    if (projectsArr[i].active === true) {
+      activeProject = projectsArr[i];
+      break;
+    }
+  }
+}
 
 function projectFactory(name) {
   let project = {
     name: name,
     active: false,
+    toDoList: [],
     addToProjects: function () {
       projectsArr.push(project);
+    },
+    addToDo: function (toDo) {
+      this.toDoList.push(toDo);
     },
   };
   return project;
 }
+
 //Add general project as default
 const general = projectFactory("general");
 general.addToProjects();
+displayProjects(projectsArr);
 
-//Adds project.name from projectsArr to DOM
 function displayProjects(array) {
   let projectList = document.getElementById("project-list");
   //Remove items from project list to avoid duplicating
@@ -22,16 +38,29 @@ function displayProjects(array) {
     projectList.removeChild(projectList.lastChild);
   }
   array.forEach((element) => {
-    projectDiv = document.createElement("div");
+    let projectDiv = document.createElement("div");
     projectDiv.textContent = element.name;
+    projectDiv.addEventListener("click", function () {
+      displayToDos(element);
+      // Remove "active" class from all projectDivs
+      projectList.querySelectorAll(".active").forEach((div) => {
+        div.classList.remove("active");
+      });
+      // Add "active" class to the clicked projectDiv
+      projectDiv.classList.add("active");
+      array.forEach((p) => {
+        if (p !== element) {
+          p.active = false;
+        }
+        element.active = true;
+        findActiveProject();
+      });
+    });
     projectList.appendChild(projectDiv);
   });
 }
 
-displayProjects(projectsArr);
-
 const projectInput = document.getElementById("project-input");
-
 const newProjectBtn = document.getElementById("add-project");
 newProjectBtn.addEventListener("click", function () {
   const newProject = projectFactory(projectInput.value);
@@ -39,3 +68,38 @@ newProjectBtn.addEventListener("click", function () {
   displayProjects(projectsArr);
   projectInput.value = "";
 });
+
+const toDoInput = document.getElementById("to-do-input");
+const newToDoBtn = document.getElementById("add-to-do");
+newToDoBtn.addEventListener("click", function () {
+  const newToDo = toDoFactory(toDoInput.value);
+  activeProject.addToDo(newToDo);
+  displayToDos(activeProject);
+  toDoInput.value = "";
+});
+
+function toDoFactory(task) {
+  let toDo = {
+    task: task,
+    done: false,
+  };
+  return toDo;
+}
+
+const makeDinner = toDoFactory("make dinner");
+general.addToDo(makeDinner);
+displayToDos(general);
+function displayToDos(project) {
+  let toDoContainer = document.getElementById("to-do-list");
+  let toDoArr = project.toDoList;
+
+  while (toDoContainer.firstChild) {
+    toDoContainer.removeChild(toDoContainer.lastChild);
+  }
+
+  toDoArr.forEach((element) => {
+    let toDo = document.createElement("div");
+    toDo.textContent = element.task;
+    toDoContainer.appendChild(toDo);
+  });
+}
